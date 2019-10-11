cae0a2c7c1e7aaeda0652f438e2dd61eb77f08fd
jakebratton28 <55863972+jakebratton28@users.noreply.github.com>
2019-10-11 13:25:56 -0400

    Update Question2.js

--- javascript/questions/Question2.js
+++ javascript/questions/Question2.js
@@ -1,7 +1,31 @@
 // Use this file to answer question 2
 
 function answer02(risk, bonus, trader) {
-    return -5
+    var total = 0;
+    for (var j = 0; j < trader.length; j++) 
+    {
+        var riskNumbers = [];
+        for (var i = 0; i < risk.length; i++) 
+        {
+            if (trader[j] >= risk[i]) 
+            {
+                riskNumbers.push(i);
+            }
+        }
+        var highest = 0;
+        for (var i = 0; i < riskNumbers.length; i++) 
+        {
+            if (bonus[riskNumbers[i]] > highest) 
+            {
+                highest = bonus[riskNumbers[i]];
+            }
+        }    
+        print(highest);
+        total = total + highest;
+    }
+    return total;
 }
 
 module.exports = answer02
+
+git push
