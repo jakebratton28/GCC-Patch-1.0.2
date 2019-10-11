82290e5b34b59382e9066d29f784d6ff6516223a
jakebratton28 <55863972+jakebratton28@users.noreply.github.com>
2019-10-11 13:30:57 -0400

    Update Question4.js

--- javascript/questions/Question4.js
+++ javascript/questions/Question4.js
@@ -1,7 +1,23 @@
 // Use this file to answer question 4
 
 function answer04(v, c, mc) {
-    return -5
+    var number = 0;
+    var index = [];
+    for (var i = c.length - 1; i >= 0; i--) {
+        number += c[i];
+        if (number <= mc) {
+            index[index.length] = i;
+        } else {
+            number -= c[i];
+        }
+    }
+    var answer = 0;
+    for (var i = 0; i < index.length; i++) {
+        answer += v[index[i]];
+    }
+    return answer;
 }
 
 module.exports = answer04
+
+git push
